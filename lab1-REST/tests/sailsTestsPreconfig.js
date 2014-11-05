b63a2159b41a5e56be6dc69f65dd39a3651863df
Matija Stepanic <stepanic.matija@gmail.com>
2014-11-05 09:30:04 +0100

    Todo E2E tests refactored

--- lab1-REST/tests/sailsTestsPreconfig.js
+++ lab1-REST/tests/sailsTestsPreconfig.js
@@ -22,9 +22,60 @@ console.log.verbose = Helper.verbose;
 function post(url, params) {
     var defer = protractor.promise.defer();
     url = sailsBaseUrl + url;
-    console.log.verbose("Calling", url);
+    console.log.verbose("POST Calling", url);
     req.post(url, params, function(error, message) {
-        console.log.verbose("Done call to", url);
+        console.log.verbose("Done POST call to", url);
+        defer.fulfill({ error: error, message: message });
+    });
+    return defer.promise;
+}
+/**
+ * Function for sending HTTP GET request
+ * @method get
+ * @param {} url
+ * @param {} params
+ * @return MemberExpression
+ */
+function get(url, params) {
+    var defer = protractor.promise.defer();
+    url = sailsBaseUrl + url;
+    console.log.verbose("GET Calling", url);
+    req.get(url, params, function(error, message) {
+        console.log.verbose("Done GET call to", url);
+        defer.fulfill({ error: error, message: message });
+    });
+    return defer.promise;
+}
+/**
+ * Function for sending HTTP PUT request
+ * @method put
+ * @param {} url
+ * @param {} params
+ * @return MemberExpression
+ */
+function put(url, params) {
+    var defer = protractor.promise.defer();
+    url = sailsBaseUrl + url;
+    console.log.verbose("PUT Calling", url);
+    req.put(url, params, function(error, message) {
+        console.log.verbose("Done PUT call to", url);
+        defer.fulfill({ error: error, message: message });
+    });
+    return defer.promise;
+}
+/**
+ * Function for sending HTTP DELETE request
+ * @method del
+ * @param {} url
+ * @param {} params
+ * @return MemberExpression
+ */
+function del(url, params) {
+    var defer = protractor.promise.defer();
+    url = sailsBaseUrl + url;
+    console.log.verbose("DELETE Calling", url);
+    req.del(url, params, function(error, message) {
+        console.log.verbose("Done DELETE call to", url);
         defer.fulfill({ error: error, message: message });
     });
     return defer.promise;
@@ -32,5 +83,8 @@ function post(url, params) {
 
 // Module exports, public API
 module.exports = {
-  post: post
+  post: post,
+  get: get,
+  put: put,
+  del: del,
 }
--- lab1-REST/tests/todo-spec.js
+++ lab1-REST/tests/todo-spec.js
@@ -2,18 +2,21 @@
 var API = require('./sailsTestsPreconfig.js');
 
 //Jasmine TESTS
-describe('sails todo Model DEMO', function() {
+describe('Sails Todo Model Test CRUD', function() {
+    // Should not reorder of tests because the order of executing is important !!!
+
+    // Config data which will be send to API, and which will be asserted
+    var data = {
+      id:       null, // ID of Model instance, to test FIND, UPDATE and DELETE
+      text:     "test to create todo task čćšđžČĆŠĐgdfgdfgdfĐŽ !@#$$%$#%^$%^",
+      duration: 78
+    };
 
     beforeEach(function() {
       // TODO: Init database (truncnate tables - prepare for testing)
     });
 
-    it('should add a todo', function() {
-        // Config data which will be send to API, and which will be asserted
-        var data = {
-          text: "test to create todo task čćšđžČĆŠĐgdfgdfgdfĐŽ !@#$$%$#%^$%^",
-          duration: 78
-        };
+    it('1 . Should Create a Todo instance', function() {
         /**
          * Request config, can be directly send to protractor.promise, but much nicer to isolate
          * @method request
@@ -41,6 +44,79 @@ describe('sails todo Model DEMO', function() {
           expect(body.createdAt).toBeDefined();
           expect(body.updatedAt).toBeDefined();
           expect(body.id).toBeDefined();
+          // Save id for other tests
+          data.id = body.id;
+        });
+    });
+
+    it('2. Should FindOne a Todo instance', function() {
+        function request() {
+          return API.get('/todo/' + data.id, {})
+        }
+        var flow = protractor.promise.controlFlow();
+        flow.execute(request).then(function(response) {
+          var result = response.message.toJSON();
+          var body = JSON.parse(result.body);
+          // Asserts
+          expect(result.statusCode).toEqual(200);
+          expect(body.text).toEqual(data.text);
+          expect(body.duration).toEqual(data.duration);
+          expect(body.createdAt).toBeDefined();
+          expect(body.updatedAt).toBeDefined();
+          expect(body.id).toEqual(data.id);
+        });
+    });
+
+    it('3. Should Update a Todo instance', function() {
+        // Change data of instance
+        data.text = "iymijenjeno ;'][:|}{^*&%&$$#@%$";
+        data.duration = 78678;
+
+        function request() {
+          return API.put('/todo/' + data.id, {
+              form : {
+                  text      : data.text,
+                  duration  : data.duration
+              }
+          })
+        }
+        var flow = protractor.promise.controlFlow();
+        flow.execute(request).then(function(response) {
+          var result = response.message.toJSON();
+          var body = JSON.parse(result.body);
+          // Asserts
+          expect(result.statusCode).toEqual(200);
+          expect(body.text).toEqual(data.text);
+          expect(body.duration).toEqual(data.duration);
+          expect(body.createdAt).toBeDefined();
+          expect(body.updatedAt).toBeDefined();
+          expect(body.id).toEqual(data.id);
+        });
+    });
+
+    it('4. Should Destroy a Todo instance', function() {
+        function request() {
+          return API.del('/todo/' + data.id, {})
+        }
+        var flow = protractor.promise.controlFlow();
+        // First request should delete and return instance object
+        flow.execute(request).then(function(response) {
+          var result = response.message.toJSON();
+          var body = JSON.parse(result.body);
+          // Asserts
+          expect(result.statusCode).toEqual(200);
+          expect(body.text).toEqual(data.text);
+          expect(body.duration).toEqual(data.duration);
+          expect(body.createdAt).toBeDefined();
+          expect(body.updatedAt).toBeDefined();
+          expect(body.id).toEqual(data.id);
+        });
+        // Second request should return error 404
+        flow.execute(request).then(function(response) {
+          var result = response.message.toJSON();
+          // Asserts
+          expect(result.statusCode).toEqual(404);
+          expect(result.body).toEqual("No record found with the specified `id`.");
         });
     });
 });
