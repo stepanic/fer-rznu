3515bfbc722a46f6f35655073390cd2a4d5c7908
Matija Stepanic <stepanic@loopus.co>
2015-01-12 13:34:54 +0100

    Init UrlCount and BrowserCount

---
+++ lab2-HADOOP/project/src/BrowserCount.java
@@ -0,0 +1,28 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+
+import java.io.IOException;
+
+public class BrowserCount {
+
+  public static void main(String[] args) throws IOException {
+    if (args.length != 2) {
+      System.err.println("Usage: VideoCount <input path> <output path>");
+      System.exit(-1);
+    }
+    JobConf conf = new JobConf(BrowserCount.class);
+    conf.setJobName("Video count");
+    FileInputFormat.addInputPath(conf, new Path(args[0]));
+    FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+    conf.setMapperClass(VideoCountMap.class);
+    conf.setReducerClass(VideoCountReduce.class);
+    conf.setOutputKeyClass(Text.class);
+    conf.setOutputValueClass(IntWritable.class);
+    JobClient.runJob(conf);
+  }
+}
---
+++ lab2-HADOOP/project/src/UrlCount.java
@@ -0,0 +1,28 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+
+import java.io.IOException;
+
+public class UrlCount {
+
+  public static void main(String[] args) throws IOException {
+    if (args.length != 2) {
+      System.err.println("Usage: UrlCount <input path> <output path>");
+      System.exit(-1);
+    }
+    JobConf conf = new JobConf(UrlCount.class);
+    conf.setJobName("URL count");
+    FileInputFormat.addInputPath(conf, new Path(args[0]));
+    FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+    conf.setMapperClass(UrlCountMap.class);
+    conf.setReducerClass(UrlCountReduce.class);
+    conf.setOutputKeyClass(Text.class);
+    conf.setOutputValueClass(IntWritable.class);
+    JobClient.runJob(conf);
+  }
+}
